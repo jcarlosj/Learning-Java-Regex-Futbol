46754c7c02b5315dd09fc3ed61f6c3e250c833ea
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-08-08 10:50:08 -0500

    Java: Refactoriza expresión regular que muestra todos los encuentros ganados por goleada

--- java_regex.java
+++ java_regex.java
@@ -6,9 +6,10 @@ import java .util .regex .Pattern;
 
 public class java_regex {
     public static void main( String [] args ) {
-        Pattern patron = Pattern .compile( "^\\d{4}\\-.*[zkZK].*$" );         // Compila la expresión regular en Java hay que escapar los \ backslaches, motivo por el que se usan dobles
+        Pattern patron = Pattern .compile( "^([\\d]{4,4})\\-[\\d]{2}\\-[\\d]{2},(.+),(.+),(\\d+),(\\d+),.*$" );         // Compila la expresión regular en Java hay que escapar los \ backslaches, motivo por el que se usan dobles
         String file = "./files/results.csv";
         String line;
+        int goleada = 0, diferencia;
 
         try {
             // Abre el archivo
@@ -19,10 +20,15 @@ public class java_regex {
 
                 // Valida si la expresión ha hecho match
                 if( matcher .find() ) {
-                    System .out .println( line );
+                    int total = Integer .parseInt( matcher .group( 4 ) ) - Integer .parseInt( matcher .group( 5 ) );
+                    diferencia = total < 0 ? total * ( -1 ) : total;
+                    if( 3 < diferencia ) {
+                        System .out .println( matcher .group( 1 ) + ": " + matcher .group( 4 ) + " - " + matcher .group( 5 ) + " | " + matcher .group( 2 ) + " Vs " + matcher .group( 3 ) );
+                        goleada++;
+                    }
                 }
-
             }
+            System .out .println( "Partidos ganados por goleada: " + goleada );
         }
         catch( Exception e ) {
             System .out .println( "Error: " + e );
