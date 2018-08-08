623a712dfd88ebb808bf163bc2aa833df71104c1
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-08-08 10:13:20 -0500

    Java: Crea expresión regular que imprime todos los encuentros de 2011

--- java_regex.java
+++ java_regex.java
@@ -1,9 +1,12 @@
 import java .io .BufferedReader;
 import java .io .FileReader;
 import java .io .IOException;
+import java .util .regex .Matcher;
+import java .util .regex .Pattern;
 
 public class java_regex {
     public static void main( String [] args ) {
+        Pattern patron = Pattern .compile( "^2011\\-.*$" );         // Compila la expresión regular en Java hay que escapar los \ backslaches, motivo por el que se usan dobles
         String file = "./files/results.csv";
         String line;
 
@@ -12,7 +15,13 @@ public class java_regex {
             BufferedReader br = new BufferedReader( new FileReader( file ) );
             // Itera cada uno de las líneas del archivo
             while( ( line = br .readLine() ) != null ) {
-                System .out .println( line );
+                Matcher matcher = patron .matcher( line );
+
+                // Valida si la expresión ha hecho match
+                if( matcher .find() ) {
+                    System .out .println( line );
+                }
+
             }
         }
         catch( Exception e ) {
